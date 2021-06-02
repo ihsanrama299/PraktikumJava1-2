25156416d321b3b37419742e6e907f31de031ccf
ihsanrama299 <ihsanramadhan299@gmail.com>
2021-06-02 16:25:36 +0800

    Project IDEA

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String namaDepan = "Muhammad";
+        String namaTengah = "Ihsan";
+        String namaBelakang = "Ramadhan";
+        int usia = 20;
+        int targetKuliah = 4;
+        double ipk = 3.89764512;
+        char nilaiAbjad = 'A';
+        boolean statusAktif = true;
+
+        System.out.print(namaDepan + namaTengah + namaBelakang);
+
+    }
+
+}
