29d998dc439b1c2966cdda5597cfa1a4ceb21a75
ihsanrama299 <ihsanramadhan299@gmail.com>
2021-06-03 14:12:59 +0800

    Operasi Tipe String

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import com.sun.org.apache.xpath.internal.objects.XString;
 
+import java.lang.reflect.Array;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -19,6 +21,22 @@ public class Main {
         String uniskaString = new String(uniskaChar);
 
         System.out.println(uniskaString);
+
+        String namaLengkap = namaDepan + " " + namaTengah + " " + namaBelakang;
+        System.out.println(namaLengkap.length());
+        System.out.println(namaLengkap.indexOf("san"));
+        System.out.println(namaLengkap.substring(5));
+        System.out.println(namaLengkap.substring(5, 12));
+        System.out.println(namaLengkap.replace("Ih", "Ha"));
+        System.out.println(namaLengkap.toUpperCase());
+        System.out.println(namaLengkap.toLowerCase());
+        System.out.println(namaLengkap.charAt(6));
+
+        String[] namaArray = namaLengkap.split(" ");
+
+        for (String nama : namaArray) {
+            System.out.println(nama);
+        }
 
     }
 
